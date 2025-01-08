8e53ceb6f6acd370d58ab5127205657fe0dd0b67
Ermias070309 <ermias.tsegaye@elev.lbs.se>
2025-01-08 15:30:21 +0100

    MovementScript
    Gå fram, bak men även hoppa (går att justera hur högt och snabbt :))

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+    public float jumpForce = 7f;
+    private bool isGrounded;
+
+    private Rigidbody2D rb;
+    private SpriteRenderer spriteRenderer;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    void Update()
+    {
+        float moveInput = Input.GetAxis("Horizontal");
+        rb.velocity = new Vector2(moveInput * moveSpeed, rb.velocity.y);
+
+        if (moveInput != 0)
+            spriteRenderer.flipX = moveInput < 0;
+
+        if (Input.GetButtonDown("Jump") && isGrounded)
+            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+            isGrounded = true;
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+            isGrounded = false;
+    }
+}
