c92a86aa8b95f55b36477db35da230e223565d00
Ermias070309 <ermias.tsegaye@elev.lbs.se>
2025-01-10 11:16:57 +0100

    Hej

--- Assets/Enemy/EnemyLockOn.cs
+++ Assets/Enemy/EnemyLockOn.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class EnemyLockOn : MonoBehaviour
 {
+    public GameObject bullet;
+    public Transform bulletPos;
+
+    private float timer;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +17,17 @@ public class EnemyLockOn : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        timer += Time.deltaTime;
+
+        if (timer > 2)
+        {
+            timer = 0;
+            shoot();
+        }
+    }
+
+    void shoot()
+    {
+        Instantiate(bullet, bulletPos.position, Quaternion.identity);
     }
 }
