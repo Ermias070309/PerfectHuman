f60c0b0eaa6a660d7b4645c119dc7815577acf34
Ermias070309 <ermias.tsegaye@elev.lbs.se>
2025-01-14 16:16:11 +0100

    Scene1

--- Assets/Enemy/EnemyLockOn.cs
+++ Assets/Enemy/EnemyLockOn.cs
@@ -20,7 +20,7 @@ public class EnemyLockOn : MonoBehaviour
     void Update()
     {
         float distance = Vector2.Distance(transform.position, player.transform.position);
-        Debug.Log(distance);
+        
 
         if(distance < 10)
         {
