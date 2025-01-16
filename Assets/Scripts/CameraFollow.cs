8b5daaf6d0762b3d72ec97fc8f0ab098a7ad4d8c
Ermias070309 <ermias.tsegaye@elev.lbs.se>
2025-01-16 20:56:21 +0100

    Uhm scene2
    och annat skit

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -1,22 +1,36 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class CameraFollow : MonoBehaviour
 {
-    public Transform target;
+    public Transform target; 
     public float smoothTime = 0.3f;
     public Vector3 offset;
 
     private Vector3 velocity = Vector3.zero;
+
+    private void Start()
+    {
+        DontDestroyOnLoad(gameObject); 
+    }
 
     void LateUpdate()
     {
         if (target != null)
         {
             Vector3 targetPosition = target.position + offset;
-            targetPosition.z = transform.position.z;
+            targetPosition.z = transform.position.z; 
             transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref velocity, smoothTime);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("NextSceneTrigger")) 
+        {
+            SceneManager.LoadScene("Scene2"); 
         }
     }
 }
