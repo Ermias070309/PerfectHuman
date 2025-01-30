f9c0b9850f0fdc2d9f852a1c129562c9b06aa5fb
Bellisen <isabelle.garcia@elev.ga.lbs.se>
2025-01-30 14:11:55 +0100

    Update Health.cs

--- Assets/Scripts/Health/Health.cs
+++ Assets/Scripts/Health/Health.cs
@@ -1,28 +1,15 @@
 using UnityEngine;
-<<<<<<< Updated upstream
 using UnityEngine.SceneManagement;
 public class Health : MonoBehaviour
 {
     [SerializeField] private float startingHealth;
     public float currentHealth { get; private set; }
  
-=======
 
-public class Health : MonoBehaviour
-{
-    [SerializeField] private float startingHealth;
->>>>>>> Stashed changes
-
-    public float currentHealth { get; private set; }
-    private Animator anim;
     private void Awake()
     {
         currentHealth = startingHealth;
-<<<<<<< Updated upstream
       
-=======
-        anim = GetComponent<Animator>();
->>>>>>> Stashed changes
     }
 
     public void TakeDamage(float _damage)
@@ -33,23 +20,13 @@ public class Health : MonoBehaviour
        
         if(currentHealth <= 0)
         {
-<<<<<<< Updated upstream
             GetComponent<PlayerMovementWithSounds>().enabled = false;
             SceneManager.LoadScene("Scene1");
             Destroy(gameObject);
-=======
-           anim.SetTrigger("hurt");
-
->>>>>>> Stashed changes
         }
      
     }
 
-<<<<<<< Updated upstream
-=======
-            anim.SetTrigger("die");
-        }
->>>>>>> Stashed changes
 
     public void AddHealth(float _value)
     {
