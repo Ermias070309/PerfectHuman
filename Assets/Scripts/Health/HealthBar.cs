7f4bcb730ad41e5b1310d82d3928b2fb41036a69
Bellisen <isabelle.garcia@elev.ga.lbs.se>
2025-01-30 14:03:53 +0100

    Update HealthBar.cs

--- Assets/Scripts/Health/HealthBar.cs
+++ Assets/Scripts/Health/HealthBar.cs
@@ -7,21 +7,20 @@ using UnityEngine.UI;
 public class HealthBar : MonoBehaviour
 {
     [SerializeField] private Health playerHealth;
-<<<<<<< Updated upstream
+
     [SerializeField] private Image totalhealthBar;
     [SerializeField] private Image currenthealthBar;
 
  private void Start()
     {
-   totalhealthBar.fillAmount = playerHealth.currentHealth / 10;
-=======
-    [SerializeField] private Health totalhealthbar;
-    [SerializeField] private Health currentealthBar;
+    totalhealthBar.fillAmount = playerHealth.currentHealth / 10;
+
+
 
     private void Start()
     {
         
->>>>>>> Stashed changes
+
     }
 
     private void Update()
